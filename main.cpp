78b176fc5f471767dae4827420eff79e42eb2804
Anmol <anmol.volt@gmail.com>
2025-09-01 11:47:51 +0530

    final

--- main.cpp
+++ main.cpp
@@ -3,58 +3,119 @@
 using namespace std;
 using json = nlohmann::json;
 
-long long toDecimal(string s, int base) {
-    long long val = 0;
-    for (char c : s) {
-        int d;
-        if (isdigit(c)) d = c - '0';
-        else d = 10 + (tolower(c) - 'a');
-        val = val * base + d;
+struct BigInt {
+    string digits;
+    BigInt(string s="0") {
+        int i = 0; while (i+1 < (int)s.size() && s[i] == '0') i++;
+        digits = s.substr(i);
     }
-    return val;
+    BigInt(long long x) { digits = to_string(x); }
+
+    static BigInt add(const BigInt &a, const BigInt &b) {
+        string A = a.digits, B = b.digits;
+        if (A.size() < B.size()) swap(A, B);
+        int carry = 0; string res = "";
+        int i = A.size()-1, j = B.size()-1;
+        while (i >= 0 || j >= 0 || carry) {
+            int x = (i >= 0 ? A[i]-'0' : 0);
+            int y = (j >= 0 ? B[j]-'0' : 0);
+            int sum = x + y + carry;
+            res.push_back(char('0' + (sum % 10)));
+            carry = sum / 10;
+            i--; j--;
+        }
+        reverse(res.begin(), res.end());
+        return BigInt(res);
+    }
+
+    static BigInt sub(const BigInt &a, const BigInt &b) { 
+        string A = a.digits, B = b.digits;
+        while (B.size() < A.size()) B = "0" + B;
+        int borrow = 0; string res="";
+        for (int i = A.size()-1; i >= 0; i--) {
+            int x = (A[i]-'0') - borrow;
+            int y = (i >= 0 ? B[i]-'0' : 0);
+            if (x < y) { x += 10; borrow = 1; }
+            else borrow = 0;
+            res.push_back(char('0' + (x-y)));
+        }
+        while (res.size() > 1 && res.back()=='0') res.pop_back();
+        reverse(res.begin(), res.end());
+        return BigInt(res);
+    }
+
+    static BigInt mul(const BigInt &a, const BigInt &b) {
+        vector<int> res(a.digits.size()+b.digits.size(), 0);
+        for (int i = a.digits.size()-1; i >= 0; i--) {
+            for (int j = b.digits.size()-1; j >= 0; j--) {
+                int p = (a.digits[i]-'0') * (b.digits[j]-'0');
+                int pos1 = i+j, pos2 = i+j+1;
+                int sum = p + res[pos2];
+                res[pos1] += sum/10;
+                res[pos2] = sum%10;
+            }
+        }
+        string s=""; for (int x: res) s.push_back(char('0'+x));
+        int k=0; while (k+1<s.size() && s[k]=='0') k++;
+        return BigInt(s.substr(k));
+    }
+
+    friend ostream& operator<<(ostream &os, const BigInt &x) {
+        os << x.digits; return os;
+    }
+};
+
+BigInt convertToDecimal(const string &value, int base) {
+    BigInt result("0");
+    for (char c : value) {
+        int digit;
+        if (isdigit(c)) digit = c - '0';
+        else digit = tolower(c) - 'a' + 10;
+        result = BigInt::mul(result, BigInt(base));
+        result = BigInt::add(result, BigInt(digit));
+    }
+    return result;
 }
 
-int main() {
-    ifstream f("input.json");
-    json j;
-    f >> j;
+vector<BigInt> multiplyPoly(const vector<BigInt> &coeffs, BigInt root) {
+    vector<BigInt> newCoeffs(coeffs.size() + 1, BigInt("0"));
+    for (size_t i = 0; i < coeffs.size(); i++) {
+        newCoeffs[i] = BigInt::add(newCoeffs[i], coeffs[i]);
+        BigInt prod = BigInt::mul(coeffs[i], root);
+        newCoeffs[i+1] = BigInt::sub(newCoeffs[i+1], prod);
+    }
+    return newCoeffs;
+}
 
-    int n = j["keys"]["n"];
-    int k = j["keys"]["k"];
+int main(int argc, char* argv[]) {
+    if (argc < 2) return 1;
+    ifstream file(argv[1]);
+    if (!file.is_open()) return 1;
 
-    vector<long long> roots;
-    for (auto& [key, val] : j.items()) {
-        if (key == "keys") continue;
-        int base;
-        if (val["base"].is_string()) base = stoi(val["base"].get<string>());
-        else base = val["base"].get<int>();
-        string value = val["value"];
-        roots.push_back(toDecimal(value, base));
+    json input;
+    file >> input;
+    int n = input["keys"]["n"];
+    int k = input["keys"]["k"];
+    int degree = k - 1;
+
+    vector<BigInt> roots;
+    for (auto it = input.begin(); it != input.end(); ++it) {
+        if (it.key() == "keys") continue;
+        string baseStr = it.value()["base"];
+        string value = it.value()["value"];
+        int base = stoi(baseStr);
+        BigInt decimal = convertToDecimal(value, base);
+        roots.push_back(decimal);
     }
 
-    vector<long long> selected(roots.begin(), roots.begin() + k);
-
-    vector<long long> coeff = {1};
-    for (auto r : selected) {
-        vector<long long> next(coeff.size() + 1, 0);
-        for (int i = 0; i < coeff.size(); i++) {
-            next[i] -= coeff[i] * r;
-            next[i + 1] += coeff[i];
-        }
-        coeff = next;
+    vector<BigInt> coeffs; coeffs.push_back(BigInt("1"));
+    for (int i = 0; i < degree && i < (int)roots.size(); i++) {
+        coeffs = multiplyPoly(coeffs, roots[i]);
     }
 
-    for (int i = coeff.size() - 1; i >= 0; i--) {
-        long long c = coeff[i];
-        if (c == 0) continue;
-        if (i != coeff.size() - 1 && c > 0) cout << "+";
-        if (i == 0) cout << c;
-        else {
-            if (c == -1) cout << "-";
-            else if (c != 1) cout << c;
-            cout << "x";
-            if (i > 1) cout << "^" << i;
-        }
+    for (int i = 0; i < (int)coeffs.size(); i++) {
+        cout << coeffs[i];
+        if (i < (int)coeffs.size()-1) cout << " ";
     }
     cout << endl;
 }
